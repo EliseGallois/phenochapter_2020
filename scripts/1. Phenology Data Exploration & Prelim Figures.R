4ecd87228c134ffd7e8685422f83c99e82bcd9d1
EliseGallois <elise.gallois94@gmail.com>
2020-04-21 13:02:37 +0100

    Elise renamed phenology cleaning script

---
+++ scripts/1. Phenology Data Exploration & Prelim Figures.R
@@ -0,0 +1,12 @@
+#### PHENOLOGY DATA EXPLORATION ####
+
+####### load necessary packages and set wd #######
+
+library(dplyr)
+library(readr)
+library(tidyr) 
+library(ggplot2)
+library(esquisse)
+
+qiki_phen_with_before_2019 <- read_csv(file = "data/qiki_phen_with_before_2019.txt")
+View(qiki_phen_with_before_2019)
